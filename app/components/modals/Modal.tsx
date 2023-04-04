7fbe8591b9d00f0a2b1d67baf22308969d121022
Tanay Vaswani <vaswani.tanay9@gmail.com>
2023-04-05 02:00:32 +0530

    Auth UI work done so far

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useEffect, useCallback, useState } from "react";
-import { IoMdClose } from 'react-icons/io';
+import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
 
 interface ModalProps {
   isOpen?: boolean;
@@ -106,7 +107,7 @@ const Modal: React.FC<ModalProps> = ({
             `}
           >
             <div
-                className="
+              className="
                     translate
                     h-full
                     lg:h-auto
@@ -123,8 +124,9 @@ const Modal: React.FC<ModalProps> = ({
                     focus:outline-none
                 "
             >
-                {/* HEADER */}
-                <div className="
+              {/* HEADER */}
+              <div
+                className="
                     flex
                     items-center
                     p-6
@@ -133,10 +135,11 @@ const Modal: React.FC<ModalProps> = ({
                     relative
                     border-b-[1px]
 
-                ">
-                    <button
-                        onClick={handleClose}
-                        className="
+                "
+              >
+                <button
+                  onClick={handleClose}
+                  className="
                         p-1
                         border-0
                         hover:opacity-70
@@ -144,13 +147,35 @@ const Modal: React.FC<ModalProps> = ({
                         absolute
                         left-9
                         "
-                    >
-                        <IoMdClose size={18} />
-                    </button>
-                    <div className="text-lg font-semibold">
-                        {title}
-                    </div>
+                >
+                  <IoMdClose size={18} />
+                </button>
+                <div className="text-lg font-semibold">{title}</div>
+              </div>
+              {/* Body */}
+              <div
+                className="
+                  relative
+                  p-6
+                  flex-auto
+                "
+              >
+                {body}
+              </div>
+              {/* Footer */}
+              <div className="flex flex-col gap-2 p-6">
+                <div
+                  className="
+                  flex 
+                  flex-row
+                  items-center
+                  gap-4
+                  w-full
+                "
+                >
+                  <Button label="My Button" />
                 </div>
+              </div>
             </div>
           </div>
         </div>
